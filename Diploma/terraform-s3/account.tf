55e13c29be75af9bcfd15e38c586de60391e81bb
RoadMania <108408885+RoadMania@users.noreply.github.com>
2024-11-15 01:32:00 +0300

    Create account.tf

---
+++ Diploma/terraform-s3/account.tf
@@ -0,0 +1,32 @@
+# Creat a service account for Terraform
+resource "yandex_iam_service_account" "service" {
+  folder_id = var.folder_id
+  name      = var.account_name
+}
+
+# Grant a role "editor" to created service account
+resource "yandex_resourcemanager_folder_iam_member" "service_editor" {
+  folder_id = var.folder_id
+  role      = "editor"
+  member    = "serviceAccount:${yandex_iam_service_account.service.id}"
+}
+
+# Create static access key for service account
+resource "yandex_iam_service_account_static_access_key" "terraform_service_account_key" {
+  service_account_id = yandex_iam_service_account.service.id
+}
+
+# Use access key to create new bucket
+resource "yandex_storage_bucket" "tf-bucket" {
+  bucket     = "bogatov-diploma-bucket"
+  access_key = yandex_iam_service_account_static_access_key.terraform_service_account_key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.terraform_service_account_key.secret_key
+
+  anonymous_access_flags {
+    read = false
+    list = false
+  }
+
+  force_destroy = true
+
+}
