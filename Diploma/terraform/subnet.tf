d8f8e14758a0117466d3400a41e252eef1ada299
RoadMania <108408885+RoadMania@users.noreply.github.com>
2024-11-16 01:35:51 +0300

    Create subnet.tf

---
+++ Diploma/terraform/subnet.tf
@@ -0,0 +1,70 @@
+locals {
+  ssh-keys = fileexists("~/.ssh/id_ed25519.pub") ? file("~/.ssh/id_ed25519.pub") : var.ssh_public_key
+  ssh-private-keys = fileexists("~/.ssh/id_ed25519") ? file("~/.ssh/id_ed25519") : var.ssh_private_key
+}
+
+resource "yandex_vpc_network" "diplom" {
+  name = var.vpc_name
+}
+resource "yandex_vpc_subnet" "diplom-subnet1" {
+  name           = var.subnet1
+  zone           = var.zone1
+  network_id     = yandex_vpc_network.diplom.id
+  v4_cidr_blocks = var.cidr1
+}
+
+resource "yandex_vpc_subnet" "diplom-subnet2" {
+  name           = var.subnet2
+  zone           = var.zone2
+  network_id     = yandex_vpc_network.diplom.id
+  v4_cidr_blocks = var.cidr2
+}
+
+variable "zone1" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+
+variable "zone2" {
+  type        = string
+  default     = "ru-central1-b"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+
+variable "cidr1" {
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "cidr2" {
+  type        = list(string)
+  default     = ["10.0.2.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "diplom"
+}
+
+variable "subnet1" {
+  type        = string
+  default     = "diplom-subnet1"
+  description = "subnet name"
+}
+
+variable "subnet2" {
+  type        = string
+  default     = "diplom-subnet2"
+  description = "subnet name"
+}
+
+data "template_file" "cloudinit" {
+ template = file("${path.module}/cloud-init.yml")
+ vars = {
+   ssh_public_key = local.ssh-keys
+   ssh_private_key = local.ssh-private-keys
+ }
+}
