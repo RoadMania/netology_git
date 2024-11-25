0734acee241c20502d4fed327cb648b72f235671
RoadMania <108408885+RoadMania@users.noreply.github.com>
2024-11-25 16:53:23 +0300

    Create variables.tf

---
+++ Diploma/terraform/variables.tf
@@ -0,0 +1,51 @@
+variable "token" {
+  type        = string
+  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
+  default     = "*********"
+}
+
+variable "cloud_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+  default     = "*********"
+}
+
+variable "folder_id" {
+  type        = string
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+  default     = "*********"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+
+variable "bucket_name" {
+  type        = string
+  default     = "bogatov-diploma-bucket"
+}
+
+variable "account_name" {
+  type        = string
+  default     = "service"
+  description = "VPC network&subnet name"
+}
+
+variable "exclude_ansible" {
+  type        = bool
+  default     = false
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key"
+  type        = string
+  default     = "******"
+}
+
+variable "ssh_private_key" {
+  description = "SSH private key"
+  type        = string
+  default     = "******"
+}
